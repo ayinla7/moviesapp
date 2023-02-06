b599aa24ffb603e113776ff7bd25a93b2f16d11f
Ayinla Kwamdeen <ayinlakwamdeen@Ayinlas-MacBook-Pro.local>
2023-02-06 14:31:07 +0000

    Main Page commit

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req, res){
-
+    res.render("home");
 });
-
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
